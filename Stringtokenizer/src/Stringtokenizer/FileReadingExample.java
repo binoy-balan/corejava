fa9668fb0c9a9b3907531742160dc41b6c90eef3
binoy-balan <57975679+binoy-balan@users.noreply.github.com>
2019-12-16 16:18:38 +0530

    Stringtokenizer

---
+++ Stringtokenizer/src/Stringtokenizer/FileReadingExample.java
@@ -0,0 +1,54 @@
+package Stringtokenizer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class FileReadingExample 
+{
+public static void main(String args[])
+{
+	BufferedReader br=null;
+	try
+	{
+		String line;
+		br=new BufferedReader(new FileReader("C:\\Users\\user_2\\Desktop\\swing\\Stringtokenizer\\src\\Stringtokenizer"));
+		while((line =br.readLine()) !=null)
+		{
+			System.out.println(line);
+			StringTokenizer StringTokenizer =new StringTokenizer(line,"|");
+			while(StringTokenizer.hasMoreElements())
+			{
+				Integer id=Integer.parseInt(StringTokenizer.nextElement().toString());
+				Double price = Double.parseDouble(StringTokenizer.nextElement().toString());
+				String username= StringTokenizer.nextElement().toString();
+				StringBuilder sb=new StringBuilder();
+				sb.append("\nd: "+id);
+				sb.append("\nPrice : "+price);
+				sb.append("\nusername : "+username);
+				sb.append("\n******************\n*");
+				System.out.println(sb);		
+			}
+		}
+		System.out.println("Done");
+	}
+	catch(IOException e)
+	{
+		e.printStackTrace();
+	}
+	finally
+	{
+		try
+		{
+			if(br !=null)
+				br.close();
+		}
+		catch(IOException ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+}
+
+}
