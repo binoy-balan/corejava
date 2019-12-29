a2efd6cbb4a2e6a58cb13312416973695d4866a0
binoy-balan <57975679+binoy-balan@users.noreply.github.com>
2019-12-29 09:35:43 +0530

    string

---
+++ String/src/StringMethods/EqualsExampleMain2.java
@@ -0,0 +1,23 @@
+package StringMethods;
+
+import java.util.ArrayList;
+
+public class EqualsExampleMain2 
+{
+public static void main(String args[])
+{
+	String str1="FACENXT";
+	ArrayList<String> list=new ArrayList<>();
+	list.add("FACE");
+	list.add("FACEPREP");
+	list.add("FACENXT");
+	list.add("FACENOW");
+	for(String str:list)
+	{
+		if(str.contentEquals(str1))
+		{
+			System.out.println("FACENXT is present");
+		}
+	}
+}
+}
